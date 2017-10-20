5f500737b9e55db38bd3cf6c573ed9da6d0fc6ba
John Quinn <johnquinn93@gmail.com>
2017-10-20 19:26:43 +0000

    Load + & - words for sentiment analysis
    Take in two .txt files, one with positive words, the other with negative.
    
    Parse the files to remove the whitespace and comments.
    
    Store each list of words into a set.
    
    See Python 3 documentation on Sets for more information.
    https://docs.python.org/3.6/tutorial/datastructures.html#sets
    
    Make the sets accessible via the Analyzer class.

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -6,7 +6,39 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
+        def load_words(txt_file):
+
+            # Create set in which to store words from the text file
+            words = set()
+
+            # Open file positive-words.txt (read)
+            file = open(txt_file, "r")
+
+            # for each line in file (each line is a word)
+            for line in file:
+                # if line starts with a semicolon, space, or linebreak
+                if str.startswith(line,";" or " " or "\n"):
+                    # loop to next line
+                    continue
+                # else this is probably a word
+                else:
+                    # strip off the trailing whitespace and linebreak
+                    word = line.rstrip("\n")
+                    # then if all that is left are alphabetic characters, assume we have a word
+                    if str.isalpha(word):
+                        # add the word to "words," the set we created earlier.
+                        words.add(word)
+
+            # close the file
+            file.close()
+
+            # return our set of words
+            return words
+
         # TODO
+        self.positives = load_words(positives)
+        self.negatives = load_words(negatives)
+
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
