e5b97a90cef3ce801f4e095a933619c3a9e01f14
John Quinn <johnquinn93@gmail.com>
2017-11-20 15:46:46 +0000

    pset8 'mashup' distribution code

---
+++ pset8-practice/apiTests/application.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# # configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+@app.route("/")
+def index():
+
+    return render_template("google-maps-tutorial.html")
