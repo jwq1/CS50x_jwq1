666b453ccf4e81f2d22bf9ba1145c97b6f86dcc0
John Quinn <johnquinn93@gmail.com>
2018-01-01 13:23:45 +0000

    Write pseudo-code to render product via .js

--- final-project/static/scripts.js
+++ final-project/static/scripts.js
@@ -6,13 +6,21 @@
 // Enforce strict js code conventions
 "use strict";
 
-// execute when the DOM is fully loaded
-$(function() {
-
-});
-
 // Render product on screen
 function displayProduct() {
+
+  // TODO: get JSON file
+
+    // TODO: loop through JSON object to get information
+
+      // TODO: loop through array of references within the JSON object
+
+    // TODO: save JSON information to variables
+
+  // TODO: render variables on screen w/ seperate function, update()
+
+
+
 
 }
 
@@ -28,5 +36,27 @@ function editProduct() {
 
 // Updates product search and/or product specific changes.
 function update() {
+
+  // TODO: use the .detch() method to remove the old product information,
+    // then replace it will the new product information.
+    // Explore whether .detach() or .empty() is more efficient.
+    // https://learn.jquery.com/using-jquery-core/manipulating-elements/#creating-new-elements
+
+  // TODO: Get element I want to change from html
+    // https://learn.jquery.com/using-jquery-core/jquery-object/#the-jquery-object
+
+  // TODO: Manipulate that element to display the product information
+
+    // TODO: Set attribute on the screen w/ attr()
+    // https://learn.jquery.com/using-jquery-core/manipulating-elements/#creating-new-elements
+      // Further detail
+      // https://learn.jquery.com/using-jquery-core/attributes/
+      // https://learn.jquery.com/using-jquery-core/working-with-selections/
+      // https://learn.jquery.com/using-jquery-core/manipulating-elements/
+
+
+
+
+
 
 };
