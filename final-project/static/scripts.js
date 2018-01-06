1437a2296e426ac23b2f882c70f2fd559e5dac5d
John Quinn <johnquinn93@gmail.com>
2018-01-06 14:05:39 +0000

    Use promise to get JSON info

--- final-project/static/scripts.js
+++ final-project/static/scripts.js
@@ -9,29 +9,12 @@
 // Render the product on screen.
 $(function retrieveJSON() {
 
-
-  // Get the JSON with $.getJSON() & Flask.url_for().
-    // Use Flask.url_for("product", parameters) to generate JSON url.
-  function getProductInfo (parameters, cb) {
-    $.getJSON(Flask.url_for("product"), parameters)
-    // A callback executed upon successful retrieval.
-    .done(function(data, textStatus, jqXHR) {
-      console.log(textStatus)
-      console.log(jqXHR)
-      cb(jqXHR)
-    })
-    // A callback executed upon failed retrieval.
-    .fail(function(data, textStatus, errorThrown) {
-      // Log an error to the browser.
-      console.log(errorThrown.toString());
-    });
-  }
-
   // Get the element id attribute when a product is clicked
   $("#recently_added_products").click(function(event) {
     // Get id of clicked element's parent
     var product_id = $(event.target).parent().attr("id");
-    console.log("element id: " + product_id)
+    console.log("element id: ")
+    console.log(product_id)
 
     // Create parameters for Flask.url_for() method.
     var parameters = {
@@ -40,18 +23,20 @@ $(function retrieveJSON() {
     console.log("parameters:")
     console.log(parameters)
 
-    // Retrieve the desired product info.
-    getProductInfo(parameters, function(productInfo) {
-      // If no JSON was found, tell the user.
-      if (typeof(productInfo) == "undefined") {
+    // Get the JSON with $.getJSON() & Flask.url_for().
+    // Use Flask.url_for("product", parameters) to generate JSON url.
+    var jsPromise = Promise.resolve(
+      $.getJSON(Flask.url_for("product"), parameters)
+    )
 
-        // TODO: Return an error message to the window.
-        console.log("404 Error: Product support is on top of this (kind of)")
-      }
-      // TODO: Otherwise, render the product using the JSON object.
-        // TODO: pass JSON to the update() function to render product info.
-
-    });
+    // Retrieve the desired product info through a promise.
+    jsPromise.then(function(response) {
+      console.log("response")
+      console.log(response)
+    }, function(xhrObj) {
+      console.log("xhrObj")
+      console.log(xhrObj)
+    })
 
   });
 
