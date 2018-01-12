fe6bb95f75170dd401248a85bac26d63ee97f639
John Quinn <johnquinn93@gmail.com>
2018-01-12 14:34:21 +0000

    Fix: Return JSON when product clicked

--- final-project/static/scripts.js
+++ final-project/static/scripts.js
@@ -6,60 +6,80 @@
 // Enforce strict js code conventions
 "use strict";
 
-// Render the product on screen.
-$(function retrieveJSON() {
+// Keep track of the products we have on screen.
+var productWasClicked = document.querySelector(".productListView")
 
-  // Get the element id attribute when a product is clicked
-  $("#recently_added_products").click(function(event) {
-    // Get id of clicked element's parent
-    var product_id = $(event.target).parent().attr("id");
-    console.log("element id: ")
+
+// Search for product information when the user selects a product.
+productWasClicked.onclick = function() {
+
+  // Get the product information our user wants.
+  retrieveJSON( getIdOnClick(productWasClicked) );
+
+}
+
+
+// Retrieve product information in the form of a JSON.
+function retrieveJSON(product_id) {
+
+    console.log(" ")
+    console.log("retreiveJSON function started")
+    console.log("product_id = ")
     console.log(product_id)
 
+  // Create parameters for Flask.url_for() method.
+  var parameters = {
+    id: product_id
+  }
+  console.log(" ")
+  console.log("retrieveJSON returned parameters:")
+  console.log(parameters)
 
-    // Create parameters for Flask.url_for() method.
-    var parameters = {
-      id: product_id
-    }
-    console.log("parameters:")
-    console.log(parameters)
+  // Get the JSON with $.getJSON() & Flask.url_for().
+  // Use Flask.url_for("product", parameters) to generate JSON url.
+  var jsPromise = Promise.resolve(
+    $.getJSON(Flask.url_for("product"), parameters)
+  )
 
-    // Get the JSON with $.getJSON() & Flask.url_for().
-    // Use Flask.url_for("product", parameters) to generate JSON url.
-    var jsPromise = Promise.resolve(
-      $.getJSON(Flask.url_for("product"), parameters)
-    )
+  // Retrieve the desired product info through a promise.
+  jsPromise.then(function(response) {
+    console.log("retrieveJSON returned response")
+    console.log(response)
+    var theProductInfo = response
+    return theProductInfo
+  }, function(xhrObj) {
+    console.log("xhrObj")
+    console.log(xhrObj)
+  })
 
-    // Retrieve the desired product info through a promise.
-    jsPromise.then(function(response) {
-      console.log("response")
-      console.log(response)
-    }, function(xhrObj) {
-      console.log("xhrObj")
-      console.log(xhrObj)
-    })
-
-  });
-
-
-});
-
+}
 
 
-// Edit product information.
-function editProduct() {
-  var jsSuccess = 'Successfully used Javascript';
+// Get the product id when it is clicked.
+function getIdOnClick(productClicked) {
 
-  alert(jsSuccess);
+  // TODO: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    // Apply await() using the Promise constructor
 
-};
+  // Get the id of a product when it is clicked.
+  var productIdOfClicked = productClicked.id
+  console.log(" ")
+  console.log("Heard click on product")
+  console.log("Heading to product page!!! but not really...")
+  console.log("node id is " + productIdOfClicked)
+
+
+  // Return the product's id.
+  return productIdOfClicked
+
+
+}
 
 
 // Redirect user to the Product page
-$(function redirectToProduct() {
+function redirectToProduct() {
 
-  // Listen for when the user clicks a product.
-  $("#recently_add_products").click(function(event) {
+  // TODO: Listen for when the user clicks a product.
 
     // TODO: Get id of the product clicked
 
@@ -67,13 +87,12 @@ $(function redirectToProduct() {
 
     // TODO: Render the product page with the selected product name and id.
 
-  });
 
-});
+};
 
 
 // Display the product information on the page
-$(function displayProduct() {
+function displayProduct() {
 
 
   // TODO: Listen for when a product page loads.
@@ -89,4 +108,4 @@ $(function displayProduct() {
       // TODO: Parse the responseJSON for the data to show on screen.
 
 
-});
+};
